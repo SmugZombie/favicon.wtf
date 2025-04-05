80b667a868746f3fb3b727a42a8cd70387d4bcd4
Ron Egli <ron@r-egli.com>
2025-04-05 15:07:47 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+const { URL } = require('url');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index', { favicon: null, error: null }));
+
+app.post('/fetch-favicon', async (req, res) => {
+  let inputUrl = req.body.url;
+  let triedHttp = false;
+  let finalUrl;
+
+  const tryFetch = async (url) => {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error('Fetch failed');
+    return response;
+  };
+
+  const tryWithProtocols = async (url) => {
+    if (!/^https?:\/\//i.test(url)) {
+      try {
+        finalUrl = `https://${url}`;
+        return await tryFetch(finalUrl);
+      } catch (err) {
+        triedHttp = true;
+        finalUrl = `http://${url}`;
+        return await tryFetch(finalUrl);
+      }
+    } else {
+      finalUrl = url;
+      return await tryFetch(finalUrl);
+    }
+  };
+
+  try {
+    const response = await tryWithProtocols(inputUrl);
+    const html = await response.text();
+    const $ = cheerio.load(html);
+    const baseUrl = new URL(finalUrl).origin;
+
+    let favicon = $('link[rel="icon"]').attr('href') ||
+                  $('link[rel="shortcut icon"]').attr('href') ||
+                  $('link[rel="apple-touch-icon"]').attr('href') ||
+                  '/favicon.ico';
+
+    if (!favicon.startsWith('http')) {
+      favicon = new URL(favicon, baseUrl).href;
+    }
+
+    res.render('index', { favicon, error: null });
+  } catch (err) {
+    const protocol = triedHttp ? 'http' : 'https';
+    console.error(`Failed to fetch favicon from ${protocol}://${inputUrl}:`, err.message);
+    res.render('index', { favicon: null, error: 'Could not retrieve favicon.' });
+  }
+});
+
+app.get('/fetch-favicon', (req, res) => {
+  // Redirect user to /
+  res.redirect('/');
+});
+
+app.listen(3000, () => console.log('Favicon tool running at http://localhost:3000'));
